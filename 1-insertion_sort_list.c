571371940742edf3d25cd13694bc21bc4d17d6bf
Josephdada01 <ridwandada500@gmail.com>
2023-09-23 17:35:57 +0100

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,114 +1,50 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * swap_nodes - a function that swap nodes in double linked list
- * @list: the list
- * @node1: the first nodes
- * @node2: the second nodes
- * Return: 0 on success
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                      in ascending order using Insertion Sort algorithm
+ * @list: Pointer to the list to be sorted
  */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
-{
-	listint_t *prev1, *prev2, *next1, *next2;
+void insertion_sort_list(listint_t **list) {
+    listint_t *sorted = NULL;  /* Initialize the sorted list */
+    listint_t *current = *list;
+    listint_t *next;
+    listint_t *temp;
+    if (list == NULL || *list == NULL || (*list)->next == NULL) {
+        return;
+    }
 
-	if (node1 == NULL || node2 == NULL || node1 == node2)
-		return;
-	prev1 = node1->prev;
-	next1 = node1->next;
-	prev2 = node2->prev;
-	next2 = node2->next;
+    while (current != NULL) {
+        next = current->next; /* Store the next node */
 
-	if (prev1 != NULL)
-	{
-		prev1->next = node2;
-	}
-	else
-	{
-		*list = node2;
-	}
-	if (next1 != NULL)
-	{
-		next1->prev = node2;
-	}
+        if (sorted == NULL || current->n <= sorted->n) {
+            /* Insert current node at the beginning of sorted list */
+            current->prev = NULL;
+            current->next = sorted;
+            if (sorted != NULL) {
+                sorted->prev = current;
+            }
+            sorted = current;
+        } else {
+            /* Traverse the sorted list to find the correct position */
+            temp = sorted;
+            while (temp->next != NULL && temp->next->n < current->n) {
+                temp = temp->next;
+            }
 
-	if (prev2 != NULL)
-	{
-		prev2->next = node1;
-	}
-	node2->prev = prev1;
-	node2->next = node1;
-	node1->prev = node2;
-	node1->next = next2;
+            /* Insert current node after temp */
+            current->prev = temp;
+            current->next = temp->next;
+            if (temp->next != NULL) {
+                temp->next->prev = current;
+            }
+            temp->next = current;
+        }
+
+        current = next;
+    }
+
+    *list = sorted; /* Update the head of the list to the sorted list */
 }
-/**
- * insert_sort_list - function that perform insert sort
- * @list: the parameter
- * Return: 0 on success
- */
-void insertion_sort_list(listint_t **list)
-{
-	listint_t *current;
-	listint_t *key;
-	listint_t *temp;
 
-	if (*list == NULL || (*list)->next == NULL)
-		return;
-	current = (*list)->next;
-
-	while (current != NULL)
-	{
-		key= current;
-
-		while (key->prev != NULL && key->n < key->prev->n)
-		{
-			swap_nodes(list, key, key->prev);
-			key = key->prev;
-		}
-		current = current->next;
-		temp = *list;
-		while (temp != NULL)
-		{
-			printf("%d", temp->n);
-			if (temp->next != NULL)
-			{
-				printf(" <-> ");
-				temp = temp->next;
-			}
-		}
-		printf("\n");
-	}
-}
-/**
- * insert_beginning - function that insert node at the beginnig
- * @list: the pointer
- * @value: the new node you are adding t to the beginnig
- * Return: 0 on success
- */
-void insert_beginning(listint_t **list, int value)
-{
-	listint_t *new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
-	{
-		perror("Memory allocation failed");
-		exit(EXIT_FAILURE);
-	}
-	initialize_node(new_node, value);
-
-	if (*list == NULL)
-	{
-		*list = new_node;
-		return;
-	}
-
-	new_node->next = *list;
-	(*list)->prev = new_node;
-	*list = new_node;
-}
-void initialize_node(listint_t *node, int value)
-{
-	int *n_ptr = (int *)&(node->n);
-	*n_ptr = value;
-	node->prev = NULL;
-	node->next = NULL;
-}
